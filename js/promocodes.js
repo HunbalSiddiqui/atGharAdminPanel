f2843c1430be8d1bb7cb733cc97586231987930d
hunbalSiddiqui <hunbal.dha@gmail.com>
2021-01-07 00:20:13 +0500

    User and products page added.

--- js/promocodes.js
+++ js/promocodes.js
@@ -9,6 +9,7 @@ function closeLoader() {
 }
 var promocodeTable = document.querySelector('.promocodeTable')
 
+
 function getAllPromoCodes() {
     loadLoader()
     axios.get('https://atghar-testing.herokuapp.com/api/promocodes')
@@ -32,11 +33,11 @@ function getAllPromoCodes() {
                         `
                 <tr>
                 <th class="flex"><button type="button" class="btn btn-danger" data-toggle="modal"
-                data-target="#deletepromo" id={${promo.name}}>Delete</button></th>
+                data-target="#deletepromo" id="${promo.name}"   onclick="deletePromoCode('${promo.name}')">Delete</button></th>
                 <th>${promo.name}</th>
-                <th>${promo.offPercentage}</th>
+                <th>${promo.offPercentage}%</th>
                 <th class="flex"><button type="button" class="btn btn-warning" data-toggle="modal"
-                data-target="#disablepromo">Disable</button></th>
+                data-target="#disablepromo" onclick="updatePromoCode('${promo._id,promo.name,promo.offPercentage}')">Disable</button></th>
             </tr>
                 `)
                 )
@@ -47,3 +48,43 @@ function getAllPromoCodes() {
         })
 }
 getAllPromoCodes()
+
+
+function deletePromoCode(promo) {
+    const token = JSON.parse(localStorage.getItem('jwt')).token
+    loadLoader()
+    axios.post(`https://atghar-testing.herokuapp.com/api/promocode/delete/${promo}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then((response) => {
+            getAllPromoCodes()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+// disable promo
+
+function updatePromoCode(promoid,promoname,promoOff) {
+    console.log(JSON.parse(promoid,promoname,promoOff))
+    const token = JSON.parse(localStorage.getItem('jwt')).token
+    loadLoader()
+    axios.put(`https://atghar-testing.herokuapp.com/api/promocode/${promoid}`, {
+            name: promoname,
+            isValid: false,
+            offPercentage: promoOff
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then((response) => {
+            getAllPromoCodes();
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
