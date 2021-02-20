507bb55788abcbb433e971d71935014a7517358a
hunbalSiddiqui <hunbal.dha@gmail.com>
2021-02-21 01:42:18 +0500

    MHS 1.2.6 ProductPage Updated For Pharmacy Products.

--- js/products.js
+++ js/products.js
@@ -57,6 +57,18 @@ var productsubCategory = document.querySelector('.productsubCategory')
 var productedit = document.querySelector('.product-edit')
 var productState = {}
 var productid = null
+var pharmacyDiv = document.querySelectorAll('.pharmacyDiv') // all pharmacy specific div
+pharmacyDiv.forEach(element => {
+    element.style.display = 'none' // hiding all the div by default. in case of grocery this will remain same, in case of pharmacy product this will be altered
+});
+var costPrice = document.querySelector('.costPrice')
+var costPriceStrip = document.querySelector('.costPriceStrip')
+var mrp = document.querySelector('.mrp')
+var mrpStrip = document.querySelector('.mrpStrip')
+var priceStrip = document.querySelector('.priceStrip')
+var usedFor = document.querySelector('.usedFor')
+var vendor = document.querySelector('.vendor')
+var company = document.querySelector('.company')
 
 function getProductDetails(productId) {
     loadLoader()
@@ -69,15 +81,32 @@ function getProductDetails(productId) {
             productedit.style.display = "grid"
             const product = response.data
             closeLoader()
+            if (product.type.toLowerCase() === 'grocery') {
+                productsubCategory.value = product.subcategory
+            } else if (product.type.toLowerCase() === 'pharmacy') {
+                productsubCategory.value = 'none'
+                pharmacyDiv.forEach(element => {
+                    element.style.display = 'flex' // displaying all pharmacy specific div
+                });
+                costPriceStrip.value = product.cpStrip
+                mrp.value = product.mrp
+                mrpStrip.value = product.mrpStrip
+                priceStrip.value = product.priceStrip
+                usedFor.value = product.usedfor
+                vendor.value = product.vendor
+                company.value = product.company
+            }
+            console.log(product)
             productid = product._id
             productname.value = product.productname
             productprice.value = product.price
+            costPrice.value = product.cp
             product.featured ?
                 featuredFlag.checked = true :
                 featuredFlag.checked = false
             producttype.value = product.type
             productCategory.value = product.category
-            productsubCategory.value = product.subcategory
+
             getProductImg(productname.value)
         })
         .catch((err) => {
@@ -113,14 +142,18 @@ function getProductImg(productName) {
             }
         })
         .then((response) => {
+            // console.log(response)
             // productimage.style.background = `url(data:image/jpeg;base64,${response.data})`
-            productimage.insertAdjacentHTML(`beforeend`,
-                `
-        <img class="w100h80" src="data:image/jpeg;base64,${response.data}"/>
-        `)
+                productimage.insertAdjacentHTML(`beforeend`,
+                    `
+            <img style="width:"100%";height:"50vh"" src="data:image/jpeg;base64,${response.data}"/>
+            `)
         })
         .catch((err) => {
-            productimage.style.background = `https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png`
+            productimage.insertAdjacentHTML(`beforeend`,
+                `
+            <img class="w100h80" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png"/>
+            `)
             console.log(err)
         })
 }
@@ -134,14 +167,26 @@ updateProductBtn.addEventListener('click', () => {
 function updateProduct() {
     loadLoader()
     // Setting product state
-    productState = {
-        productname: productname.value,
-        price: productprice.value,
-        featured: featuredFlag.checked,
-        category: productCategory.value,
-        type: producttype.value,
-        subcategory: productsubCategory.value
+    if (producttype.value.toLowerCase() === 'grocery') {
+        productState = {
+            productname: productname.value,
+            price: productprice.value,
+            featured: featuredFlag.checked,
+            category: productCategory.value,
+            type: producttype.value,
+            subcategory: productsubCategory.value
+        }
+    } else if (producttype.value.toLowerCase() === 'pharmacy') {
+        // TODO : add all the fields in productState which shall be sent while updating.
+        productState = {
+            productname: productname.value,
+            price: productprice.value,
+            featured: featuredFlag.checked,
+            category: productCategory.value,
+            type: producttype.value,
+        }
     }
+
     if (productid) {
         const admin = JSON.parse(localStorage.getItem('jwt')).user
         const token = JSON.parse(localStorage.getItem('jwt')).token
@@ -164,9 +209,10 @@ function updateProduct() {
 
 // Delete
 const removeproduct = document.querySelector('.removeproduct')
-removeproduct.addEventListener('click',()=>{
+removeproduct.addEventListener('click', () => {
     deleteProduct()
 })
+
 function deleteProduct() {
     loadLoader()
     const token = JSON.parse(localStorage.getItem('jwt')).token
