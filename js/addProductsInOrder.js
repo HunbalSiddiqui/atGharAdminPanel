9f2ebf1ad4e109f34ac4fcf503e14e8e77033d38
hunbalSiddiqui <hunbal.dha@gmail.com>
2021-03-26 15:27:49 +0500

    Development rider password message updated.

--- js/addProductsInOrder.js
+++ js/addProductsInOrder.js
@@ -1,5 +1,5 @@
-// var HEROKU_API = 'https://atghar-testing.herokuapp.com/api'
-var HEROKU_API = 'https://www.atghar.com/api'
+var HEROKU_API = 'https://atghar-testing.herokuapp.com/api'
+// var HEROKU_API = 'https://www.atghar.com/api'
 // var API = 'https://www.atghar.com/api'
 
 const orderId = window.location.hash.split('#')[1]
